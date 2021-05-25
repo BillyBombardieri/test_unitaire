05ed26ae9e5bbce47df550a510360eeacd2c50cb
Billy Bombardieri <billy.bombardieri@energisme.com>
2021-05-25 14:34:04 +0200

    Add " - , * , / "

--- PHPUnit/Calculatrice.php
+++ PHPUnit/Calculatrice.php
@@ -5,4 +5,16 @@ class Calculatrice {
     public function addition($nb1, $nb2) {
         return $nb1 + $nb2;
     }
+
+    public function soustraction($nb1, $nb2) {
+        return $nb1 - $nb2;
+    }
+
+    public function multi($nb1, $nb2) {
+        return $nb1 * $nb2;
+    }
+
+    public function divi($nb1, $nb2) {
+        return $nb1 / $nb2;
+    }
 }
--- PHPUnit/CalculatriceTest.php
+++ PHPUnit/CalculatriceTest.php
@@ -14,4 +14,22 @@ class CalculatriceTest extends TestCase {
         $this->assertEquals(40, $result);
     }
 
+    public function testSoustraction() {
+        $calc = new Calculatrice();
+        $result = $calc->soustraction(20, 5);
+        $this->assertEquals(15, $result);
+    }
+
+    public function testMultiplication() {
+        $calc = new Calculatrice();
+        $result = $calc->multi(5, 25);
+        $this->assertEquals(125, $result);
+    }
+
+    public function testDivision() {
+        $calc = new Calculatrice();
+        $result = $calc->divi(25, 5);
+        $this->assertEquals(5, $result);
+    }
+
 }
